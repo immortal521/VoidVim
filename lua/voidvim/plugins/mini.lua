c33adb181382fdc0345ecebaf8e7f20cbd7b761b
immortal521 <immort521@gmail.com>
2025-09-02 21:23:51 +0800

    feat: add and configure mini plugins
    - Add mini.move, mini.ai, and mini.pairs
    
    - Configure mini.surround with custom mappings
    
    - Setup mini.icons with glyph styles
    
    - Extend mini.diff with toggle command and sign styles

--- lua/voidvim/plugins/mini.lua
+++ lua/voidvim/plugins/mini.lua
@@ -1,4 +1,64 @@
 return {
   { "nvim-mini/mini-git", version = false, main = "mini.git", opts = {} },
-  { "nvim-mini/mini.diff", version = false, opts = {} },
+  { "nvim-mini/mini.move", version = false, opts = {} },
+  { "nvim-mini/mini.ai", version = false, opts = {} },
+  {
+    "nvim-mini/mini.diff",
+    version = false,
+    keys = {
+      {
+        "<leader>go",
+        function()
+          require("mini.diff").toggle_overlay(0)
+        end,
+        desc = "Toggle mini.diff overlay",
+      },
+    },
+    opts = {
+      view = {
+        style = "sign",
+        signs = {
+          add = "▎",
+          change = "▎",
+          delete = "",
+        },
+      },
+    },
+  },
+  { "nvim-mini/mini.pairs", version = false, opts = {} },
+  {
+    "nvim-mini/mini.surround",
+    version = false,
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+
+        suffix_last = "l", -- Suffix to search with "prev" method
+        suffix_next = "n", -- Suffix to search with "next" method
+      },
+    },
+  },
+  {
+    "nvim-mini/mini.icons",
+    version = false,
+    opts = {
+      style = "glyph",
+
+      file = {
+        README = { glyph = "󰆈", hl = "MiniIconsYellow" },
+        ["README.md"] = { glyph = "󰆈", hl = "MiniIconsYellow" },
+      },
+      filetype = {
+        bash = { glyph = "󱆃", hl = "MiniIconsGreen" },
+        sh = { glyph = "󱆃", hl = "MiniIconsGrey" },
+        toml = { glyph = "󱄽", hl = "MiniIconsOrange" },
+      },
+    },
+  },
 }
